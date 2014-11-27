8ad49ae394bf1e9d0918481b7f3a44a4833398bd
myhnuhai <myhnuhai@163.com>
2014-11-27 14:03:24 +0800

    Map与JavaBean相互转换

---
+++ src/main/java/com/jsjty/util/BeanUtil.java
@@ -0,0 +1,62 @@
+package com.jsjty.util;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by 马英虎 on 2014/11/27.
+ */
+
+/**
+ * Map 与 JavaBean相互转换
+ */
+public class BeanUtil {
+
+    public static Map<String, Object> bean2Map(Object obj) {
+
+        if(obj == null){
+            return null;
+        }
+        Map<String, Object> map = new HashMap<String, Object>();
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+            PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+            for (PropertyDescriptor property : propertyDescriptors) {
+                String key = property.getName();
+                // 过滤class属性
+                if (!key.equals("class")) {
+                    // 得到property对应的getter方法
+                    Method getter = property.getReadMethod();
+                    Object value = getter.invoke(obj);
+                    map.put(key, value);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("bean2Map Error " + e);
+        }
+        return map;
+    }
+
+    public static void map2Bean(Map<String, Object> map, Object obj) {
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+            PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+
+            for (PropertyDescriptor property : propertyDescriptors) {
+                String key = property.getName();
+
+                if (map.containsKey(key)) {
+                    Object value = map.get(key);
+                    Method setter = property.getWriteMethod();
+                    setter.invoke(obj, value);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("map2Bean Error " + e);
+        }
+    }
+}
